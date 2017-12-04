ef89d0c217aad5189235adda7bac4724ab1e871a
Bradl <Bradl@192.168.1.105>
2017-12-04 02:06:58 -0500

    Added Json parser and schema for programming Turing machines.

---
+++ Turing/src/Parser.java
@@ -0,0 +1,16 @@
+import java.io.File;
+
+import com.example.types.TuringCode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+public class Parser {
+
+	public static void main(String[] args) throws Exception {
+		ObjectMapper om = new ObjectMapper();
+		TuringCode tc = om.readValue(new File("src/resources/code/test.json"), TuringCode.class);
+		
+		System.out.println(tc);
+
+	}
+
+}
